e6ccb376665885227fe353b6b350c368203de52f
Gustav Fåhraeus <g.fahraeus@hotmail.com>
2022-05-15 18:00:33 +0200

    second sesh sunday

--- agents/dqlearning_agent.py
+++ agents/dqlearning_agent.py
@@ -7,34 +7,39 @@ class Agent(object): #Keep the class name!
         self.action_space = action_space
         self.state_space = state_space
         
-        self.learning_rate = 0.1
-        self.future_discount_factor = 1
-        self.qa_table = np.ones([self.state_space, self.action_space])
-        self.qb_table = np.ones([self.state_space, self.action_space])
+        self.learning_rate = 0.4
+        self.future_discount_factor = 0.9
+        self.qa_table = 0.5*np.ones([self.state_space, self.action_space])
+        self.qb_table = 0.5*np.ones([self.state_space, self.action_space])
         
         self.epsilon = 0.05
         self.latest_state = None
+        self.latest_action = None
 
 
     def observe(self, observation, reward, done):
-        if random.uniform(0,1) > 0.5:
-            a_star = np.argmax(self.qa_table[observation]) #hmmm
-            future = (self.future_discount_factor * self.qb_table[observation, a_star] - self.qa_table[self.latest_state,self.action] )if not done else 0
-            self.qa_table[self.latest_state, self.action] = self.qa_table[self.latest_state,self.action] + self.learning_rate * \
-                (reward + future)
+        if random.uniform(0,1) > 0.5: # randomly choose either table a or b
+            a_star = np.argmax(self.qa_table[observation])
+            if done:
+                potential_future_value = 0
+            else: 
+                potential_future_value = self.future_discount_factor * self.qb_table[observation, a_star] \
+                    - self.qa_table[self.latest_state,self.latest_action]
+            self.qa_table[self.latest_state, self.latest_action] = self.qa_table[self.latest_state,self.latest_action] \
+                + self.learning_rate * (reward + potential_future_value)
         else:
-            a_star = np.argmax(self.qb_table[observation]) #hmmm
-            future = (self.future_discount_factor * self.qa_table[observation, a_star] - self.qb_table[self.latest_state,self.action] )if not done else 0
-            self.qb_table[self.latest_state, self.action] = self.qb_table[self.latest_state,self.action] + self.learning_rate * \
-                (reward + future )    
+            a_star = np.argmax(self.qb_table[observation])
+            potential_future_value = self.future_discount_factor * self.qa_table[observation, a_star] \
+                - self.qb_table[self.latest_state,self.latest_action] 
+            self.qb_table[self.latest_state, self.latest_action] = self.qb_table[self.latest_state,self.latest_action] \
+                + self.learning_rate * (reward + potential_future_value)    
 
  
     def act(self, observation):
         self.latest_state = observation
-        #print(self.q_table)
         if random.uniform(0, 1) <= self.epsilon:
-            self.action = np.random.randint(self.action_space) # Explore
+            self.latest_action = np.random.randint(self.action_space) # Explore
         else:
-            self.action = np.argmax(self.qa_table[observation] + self.qa_table[observation]) # Exploit
+            self.latest_action = np.argmax(self.qa_table[observation] + self.qa_table[observation]) # Exploit
 
-        return self.action
+        return self.latest_action
